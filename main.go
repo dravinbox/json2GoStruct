70a7151127fcefd74d20591d5e84d05f134919d7
ZhangRongYan <kawai566@yeah.net>
2019-01-22 10:12:26 +0800

    v1

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/dravinbox/json2GoStruct/controller"
 	"github.com/dravinbox/json2GoStruct/util"
 )
 
+var (
+ json = flag.String("json","{}","input json string")
+
+)
+
+
+
 
 
 func main() {
 
-	var str ="{\"user_name\":\"zry\",\"age\":18,\"profile\":{\"color\":\"red\",\"size\":12},\"tel\":[],\"address\":[{\"city\":\"Guangzhou\",\"province\":\"Guangdong\"}]}"
+	//var str ="{\"user_name\":\"zry\",\"age\":18,\"profile\":{\"color\":\"red\",\"size\":12},\"tel\":[],\"address\":[{\"city\":\"Guangzhou\",\"province\":\"Guangdong\"}]}"
+	flag.Parse()
 
-	jsonMap, e := util.Json2Map(str)
+	//fmt.Println(*json)
+	jsonMap, e := util.Json2Map(*json)
+	//jsonMap, e := util.Json2Map(str)
+
 	if e !=nil {
 		fmt.Println(e)
 		return
